76c4519f61ba3195ef9f495dbcf3c03561c83aff
vtsokas <vtsokas@gmail.com>
2020-10-20 12:02:33 +0300

    Add files via upload

---
+++ data/cache/module-config-cache.application.config.cache.php
@@ -0,0 +1,82 @@
+<?php
+return array (
+  'service_manager' => 
+  array (
+    'aliases' => 
+    array (
+      'HttpRouter' => 'Zend\\Router\\Http\\TreeRouteStack',
+      'router' => 'Zend\\Router\\RouteStackInterface',
+      'Router' => 'Zend\\Router\\RouteStackInterface',
+      'RoutePluginManager' => 'Zend\\Router\\RoutePluginManager',
+      'ValidatorManager' => 'Zend\\Validator\\ValidatorPluginManager',
+    ),
+    'factories' => 
+    array (
+      'Zend\\Router\\Http\\TreeRouteStack' => 'Zend\\Router\\Http\\HttpRouterFactory',
+      'Zend\\Router\\RoutePluginManager' => 'Zend\\Router\\RoutePluginManagerFactory',
+      'Zend\\Router\\RouteStackInterface' => 'Zend\\Router\\RouterFactory',
+      'Zend\\Validator\\ValidatorPluginManager' => 'Zend\\Validator\\ValidatorPluginManagerFactory',
+    ),
+  ),
+  'route_manager' => 
+  array (
+  ),
+  'router' => 
+  array (
+    'routes' => 
+    array (
+      'home' => 
+      array (
+        'type' => 'Zend\\Router\\Http\\Literal',
+        'options' => 
+        array (
+          'route' => '/',
+          'defaults' => 
+          array (
+            'controller' => 'Application\\Controller\\IndexController',
+            'action' => 'index',
+          ),
+        ),
+      ),
+      'application' => 
+      array (
+        'type' => 'Zend\\Router\\Http\\Segment',
+        'options' => 
+        array (
+          'route' => '/application[/:action]',
+          'defaults' => 
+          array (
+            'controller' => 'Application\\Controller\\IndexController',
+            'action' => 'index',
+          ),
+        ),
+      ),
+    ),
+  ),
+  'controllers' => 
+  array (
+    'factories' => 
+    array (
+      'Application\\Controller\\IndexController' => 'Zend\\ServiceManager\\Factory\\InvokableFactory',
+    ),
+  ),
+  'view_manager' => 
+  array (
+    'display_not_found_reason' => true,
+    'display_exceptions' => true,
+    'doctype' => 'HTML5',
+    'not_found_template' => 'error/404',
+    'exception_template' => 'error/index',
+    'template_map' => 
+    array (
+      'layout/layout' => 'C:\\projects\\zf2-tutorial\\module\\Application\\config/../view/layout/layout.phtml',
+      'application/index/index' => 'C:\\projects\\zf2-tutorial\\module\\Application\\config/../view/application/index/index.phtml',
+      'error/404' => 'C:\\projects\\zf2-tutorial\\module\\Application\\config/../view/error/404.phtml',
+      'error/index' => 'C:\\projects\\zf2-tutorial\\module\\Application\\config/../view/error/index.phtml',
+    ),
+    'template_path_stack' => 
+    array (
+      0 => 'C:\\projects\\zf2-tutorial\\module\\Application\\config/../view',
+    ),
+  ),
+);
